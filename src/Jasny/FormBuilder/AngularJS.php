2a9559cf87489c66b01666e21d0b8ddc8cb6a104
Arnold Daniels <arnold@jasny.net>
2014-07-05 23:53:01 +0200

    First commit

---
+++ src/Jasny/FormBuilder/AngularJS.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Jasny\FormBuilder;
+
+use Jasny\FormBuilder;
+
+/**
+ * Render element for use with AngularJS.
+ * Optionaly use features from Jasny AngularJS.
+ * 
+ * @link http://getbootstrap.com
+ * @link http://jasny.github.io/bootstrap
+ * 
+ * @option int version  Which major AngularJS version is used
+ */
+class AngularJS extends Decorator
+{
+    protected $deep = true;
+    
+    /**
+     * Apply default modifications.
+     * 
+     * @param Element $element
+     */
+    public function apply($element)
+    {
+        if ($element instanceof Control) {
+            if (!isset($element->attr['ng-model'])) $element->attr['ng-model'] = \Closure::bind(function() {
+                $ctrl = preg_replace('/^.*\s+as\s+/', '', $this->getOption('controller'));
+                return (isset($ctrl) ? $ctrl . '.' : '') . $this->getName();
+            }, $element);
+        }
+    }
+    
+    /**
+     * Register Boostrap decorator and elements
+     * 
+     * @return 
+     */
+    public static function register()
+    {
+        FormBuilder::$decorators['angularjs'] = ['Jasny\FormBuilder\AngularJS'];
+    }
+}
